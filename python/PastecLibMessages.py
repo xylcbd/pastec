c982156b8d29350126d1b96654324c00d810d332
Adrien Maglo <magsoft@videolan.org>
2013-12-16 17:13:05 +0100

    Fix a wrong commit about the communication messages

--- python/PastecLibMessages.py
+++ python/PastecLibMessages.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 class Reply():
-    INIT_BUILD_FORWARD_INDEX = 1
-    BUILD_BACKWARD_INDEX = 2
-    INIT_SEARCH = 3
-    INDEX = 10
-    INDEX_IMAGE = 11
-    PING = 20
-    STOP = 30
-    SEARCH = 40
+    OK = 1
+    ERROR_GENERIC = 2
+    PONG = 3
+    WRONG_MODE = 10
+    IMAGE_DATA_TOO_BIG = 20
+    IMAGE_NOT_INDEXED = 21
     
 class Query():
     INIT_BUILD_FORWARD_INDEX = 1
     BUILD_BACKWARD_INDEX = 2
+    INIT_SEARCH = 3
     INDEX = 10
     INDEX_IMAGE = 11
     PING = 20
