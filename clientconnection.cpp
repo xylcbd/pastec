28f4f3f497dc4b7205cc9c550f653d2bb3658d24
Adrien Maglo <magsoft@videolan.org>
2014-03-20 17:29:09 +0100

    Increase the timeout limit.

--- clientconnection.cpp
+++ clientconnection.cpp
@@ -49,7 +49,7 @@ ClientConnection::~ClientConnection()
 void *ClientConnection::run()
 {
     /* Prepare the usage of poll */
-    int timeout = 30 * 1000; // 30 sec.
+    int timeout = 86400 * 1000; // one day.
     struct pollfd fds[2];
     fds[0].fd = socketFd;
     fds[0].events = POLLIN;
