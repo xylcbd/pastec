298592497b0a9e80a1c71e905df8b045084b63fc
Adrien Maglo <magsoft@videolan.org>
2014-04-07 09:06:17 +0200

    Allow to specify the host of the startSearch script

--- python/startSearch.py
+++ python/startSearch.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 import PastecLib
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: startSearch serverHost")
 
 pastec = PastecLib.PastecConnection()
 
-pastec.connect()
+pastec.connect(sys.argv[1])
 pastec.initSearch()
 
 pastec.close()
