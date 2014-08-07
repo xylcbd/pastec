13ca0a880fd971bb36dd490ac3f8f5a435a22b36
Adrien Maglo <magsoft@videolan.org>
2014-08-07 17:49:38 +0200

    PastecLib: update the python API to the new Json interface

--- python/PastecLib.py
+++ python/PastecLib.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-import socket
-import struct
-
-from PastecLibMessages import Reply, Query
-
-MAX_IMAGE_SIZE = 1024 * 1024
+import urllib.request
+import json
 
 
 class PastecException(Exception):
@@ -17,123 +13,77 @@ class PastecException(Exception):
 
 class PastecConnection:
 
-    def sendData(self, data):
-        totalsent = 0
-        while totalsent < len(data):
-            sent = self.sock.send(data[totalsent:])
-            if sent == 0:
-                raise RuntimeError("Socket connection broken")
-            totalsent = totalsent + sent
+    def __init__(self, pastecHost = "localhost", pastecPort = 4213):
+        self.host = pastecHost
+        self.port = pastecPort
 
-    def connect(self, host="localhost", port=4212):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setblocking(True)
-        self.sock.connect((host, port))
-        val = self.waitForReply()
-        self.raiseExceptionIfNeeded(val)
-
-    def close(self):
-        self.sock.close()
-
-    def waitForReply(self):
-        d = self.sock.recv(1024)
-        if len(d) > 4:
-            raise PastecException("Received message too long.")
-        val = struct.unpack("I", d)[0]
-        return val
+    def request(self, path, method, data = None):
+        req = urllib.request.Request(url="http://" + self.host + ":" + \
+                                     str(self.port) + "/" + path, \
+                                     data=data, method=method)
+        f = urllib.request.urlopen(req)
+        ret = f.read().decode()
+        return json.loads(ret)
 
     def indexImageFile(self, imageId, filePath):
         self.indexImageData(imageId, self.loadFileData(filePath))
 
     def indexImageData(self, imageId, imageData):
-        if len(imageData) > MAX_IMAGE_SIZE:
-            raise PastecException("Image data size too big.")
-
-        d = struct.pack("III", Query.INDEX_IMAGE, \
-                        imageId, len(imageData))
-        d += imageData
-
-        self.sendData(d)
-
-        val = self.waitForReply()
-        self.raiseExceptionIfNeeded(val)
+        ret = self.request("index/images/" + str(imageId), "PUT", imageData)
+        self.raiseExceptionIfNeeded(ret["type"])
 
     def removeImage(self, imageId):
-        d = struct.pack("II", Query.REMOVE_IMAGE, imageId)
-        self.sendData(d)
-        val = self.waitForReply()
-        self.raiseExceptionIfNeeded(val)
+        ret = self.request("index/images/" + str(imageId), "DELETE", imageData)
+        self.raiseExceptionIfNeeded(ret["type"])
 
-    def writeIndex(self):
-        d = struct.pack("I", Query.WRITE_INDEX)
-        self.sendData(d)
-        val = self.waitForReply()
-        self.raiseExceptionIfNeeded(val)
+    def loadIndex(self, path = ""):
+        s = json.dumps({"type" : "LOAD", "index_path" : path})
+        ret = self.request("index/io", "POST", bytearray(s, "UTF-8"))
+        self.raiseExceptionIfNeeded(ret["type"])
+
+    def writeIndex(self, path = ""):
+        s = json.dumps({"type" : "WRITE", "index_path" : path})
+        ret = self.request("index/io", "POST", bytearray(s, "UTF-8"))
+        self.raiseExceptionIfNeeded(ret["type"])
 
     def clearIndex(self):
-        d = struct.pack("I", Query.CLEAR_INDEX)
-        self.sendData(d)
-        val = self.waitForReply()
-        self.raiseExceptionIfNeeded(val)
+        s = json.dumps({"type" : "CLEAR"})
+        ret = self.request("index/io", "POST", bytearray(s, "UTF-8"))
+        self.raiseExceptionIfNeeded(ret["type"])
 
     def imageQueryFile(self, filePath):
         return self.imageQueryData(self.loadFileData(filePath))
 
     def imageQueryData(self, imageData):
-        if len(imageData) > MAX_IMAGE_SIZE:
-            raise PastecException("Image data size too big.")
-
-        d = struct.pack("II", Query.SEARCH, len(imageData))
-        d += imageData
-
-        self.sendData(d)
-
-        msg = b""
-        while len(msg) < 4:
-            msg += self.sock.recv(1024)
-
-        # Get the message code.
-        val = struct.unpack("I", msg[:4])[0]
-        self.raiseExceptionIfNeeded(val)
-
-        # We get a list of images.
-
-        while len(msg) < 8:
-            msg += self.sock.recv(1024)
-
-        # Get the number of images.
-        nbImages = struct.unpack("I", msg[4:8])[0]
-
-        # Receive all the message containing the ids of the images.
-        while len(msg) < 5 + nbImages * 4:
-            msg += s.recv(1024)
-
-        # Extract the image ids.
-        imageIds = []
-        for i in range(nbImages):
-            imageIds += [struct.unpack("I", msg[8 + 4 * i : 8 + 4 * (i + 1)])[0]]
-
+        ret = self.request("index/searcher", "POST", imageData)
+        self.raiseExceptionIfNeeded(ret["type"])
+        imageIds = ret["image_ids"]
         return imageIds
 
     def raiseExceptionIfNeeded(self, val):
-        if val == Reply.OK:
-            return
-        elif val == Reply.ERROR_GENERIC:
+        if val == "ERROR_GENERIC":
             raise PastecException("Generic error.")
-        elif val == Reply.TOO_MANY_CLIENTS:
+        elif val == "TOO_MANY_CLIENTS":
             raise PastecException("Too many clients connected to the server.")
-        if val == Reply.IMAGE_DATA_TOO_BIG:
+        elif val == "IMAGE_DATA_TOO_BIG":
             raise PastecException("Image data size too big.")
-        elif val == Reply.IMAGE_SIZE_TOO_BIG:
+        elif val == "IMAGE_NOT_INDEXED":
+            raise PastecException("Image not indexed.")
+        elif val == "IMAGE_SIZE_TOO_BIG":
             raise PastecException("Image dimenssions too big.")
-        elif val == Reply.IMAGE_SIZE_TOO_SMALL:
-            raise PastecException("Image size too small.")
-        elif val == Reply.IMAGE_NOT_DECODED:
+        elif val == "IMAGE_NOT_DECODED":
             raise PastecException("The query image could not be decoded.")
-        elif val == Reply.IMAGE_ALREADY_IN_INDEX:
+        elif val == "IMAGE_SIZE_TOO_SMALL":
+            raise PastecException("Image size too small.")
+        elif val == "IMAGE_ALREADY_IN_INDEX":
             raise PastecException("An image with this id already exists in the index.")
-        else:
-            raise PastecException("Unkown error code: %#010x" % val)
+        elif val == "IMAGE_NOT_FOUND":
+            raise PastecException("Image not found.")
+
+        elif val == "INDEX_NOT_FOUND":
+            raise PastecException("Index not found.")
+        elif val == "INDEX_NOT_WRITTEN":
+            raise PastecException("Index not written.")
 
     def loadFileData(self, filePath):
         fd = open(filePath, "rb")
